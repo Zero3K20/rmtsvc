69dc8cbd73d76d68a9595a16a352e60a236c7cf0
Zero3K20 <zero3k2000@proton.me>
2026-04-05 22:42:08 -0400

    Delete Downpour directory

--- Downpour/mtTorrent/Core/Api/ListenerImpl.cpp
+++
@@ -1,6 +0,0 @@
-#include "IncomingPeersListener.h"
-
-std::string mttApi::PortListener::getUpnpReadableInfo() const
-{
-	return static_cast<const mtt::IncomingPeersListener*>(this)->getUpnpReadableInfo();
-}
